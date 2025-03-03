3e62ce289fca057a6b92e8aff3cd2de73b6e5c1c
Paul Marunko <marunkopavlo@gmail.com>
2025-03-02 19:26:01 -0700

    f1

--- config/settings.py
+++ config/settings.py
@@ -88,14 +88,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myapp',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
+        #'USER': 'postgres',
+        #'PASSWORD': 'admin',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
-
+ 
+ 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
