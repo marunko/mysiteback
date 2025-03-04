3e282e290b297b919b3de7ff23dfba6f20507bcc
Paul Marunko <marunkopavlo@gmail.com>
2025-03-03 17:45:42 -0700

    f1

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),  # Include API URLs
+    path('', include('myapp.urls')),  # Include API URLs
 ]
